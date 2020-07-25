d2ee2c1bb1cf71687bcf7bdcd59aadf918a0c7b8
samprasgit <by14274044@gmail.com>
2020-07-25 23:49:21 +0800

    python  异常处理

--- Python基础知识梳理/Pyhton-Language/test.py
+++ Python基础知识梳理/Pyhton-Language/test.py
@@ -1,48 +1,19 @@
-# print("1500-2700之间可以同时整除5和7的数字：")
-# for i in range(1500, 2700):
-#     nums = i
-#     if nums % 5 == 0 and nums % 7 == 0:
-#         print(nums)
-
-
-contest = input(
-    "请依次输入v1，v2，t，s，l（空格隔开）其中(v1,v2< =100;t< =300;s< =10;l< =10000且为v1,v2的公倍数)").split()
-
-
-'''
-v1:兔子速度
-v2:乌龟速度
-t:兔子领先距离
-s:兔子休息时间
-l:赛道长度
-'''
-
-v1, v2, t, s, l = (int(i) for i in contest)
-time, tl, rl = 0, 0, 0
+import random
+a = int(random.randint(1, 100))
+print("猜测1-100的一个数字")
+i = 1
 while True:
-    if rl - tl >= t:
-        for i in range(s):
-            tl += v2
-            time += 1
-            if tl >= l > tl:
-                print('T')
-                print(time)
-                exit()
-
-    else:
-        rl += v1
-        tl += v2
-        time += 1
-
-    if rl >= l > tl:
-        print("R")
-        print(time)
-        break
-    elif tl >= l > rl:
-        print("T")
-        print(time)
-        break
-    elif rl == tl >= l:
-        print("D")
-        print(time)
-        break
+    x = input('第%d次猜测，请输入一个数字：' % i)
+    try:
+        if type(eval(x)) == int:
+            guessnum = int(x)
+            if guessnum < a:
+                print('小了！')
+            elif guessnum > a:
+                print("大了！")
+            else:
+                print("恭喜您猜到了，这个数是%d" % a)
+                break
+    except:
+        print("输入无效")
+    i += 1
