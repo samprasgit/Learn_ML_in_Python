34b213d5982933755a3e91b364e5373ea4aadafa
samprasgit <by14274044@gmail.com>
2020-08-06 18:46:53 +0800

    Update

--- Python基础知识梳理/Pyhton-Language/test.py
+++ Python基础知识梳理/Pyhton-Language/test.py
@@ -1,17 +1,3 @@
-
-
-class Ticket:
-
-    def __init__(self, kid, adult, day):
-        self.kid = kid
-        self.adult = adult
-        self.day = day
-
-    def prices(self):
-        if self.day in range(6):
-            print(100 * self.adult + 50 * self.kid)
-        else:
-            print(1.2 * (100 * self.adult + 50 * self.kid))
-
-m = Ticket(1, 2, 1)
-m.prices()
+Links = {'B': '百度', 'A': '阿里', 'T': '腾讯'}
+for each in Links:
+    print('%s -> %s' % (each, Links[each]))
