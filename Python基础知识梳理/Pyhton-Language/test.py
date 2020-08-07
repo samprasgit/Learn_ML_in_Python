3d1dc989e2cb21a2c05a229a8135daa18017acc8
samprasgit <by14274044@gmail.com>
2020-08-07 17:14:59 +0800

    Update

--- Python基础知识梳理/Pyhton-Language/test.py
+++ Python基础知识梳理/Pyhton-Language/test.py
@@ -1,3 +1,32 @@
-Links = {'B': '百度', 'A': '阿里', 'T': '腾讯'}
-for each in Links:
-    print('%s -> %s' % (each, Links[each]))
+# 查询任意年内所有周日
+import parser
+import datetime
+
+a = input('请输入年份:')
+# print(type(a))
+
+
+def all_sundays(year):
+    a0 = int(a)
+    dt1 = datetime.date(a0, 1, 1)
+    dt2 = datetime.date(a0, 12, 31)
+    # print(dt1,dt2)
+    dt = (dt2 - dt1).days
+    '''zhou=(dt//7)#周数
+    zhouji=dt1.isoweekday()#判断当前周几
+    cha=7-zhouji
+    td_cha=datetime.timedelta(days=cha)
+    first_sunday=dt1+td_cha#第一个周日
+    print(first_sunday)'''
+    # td_cha7=datetime.timedelta(days=7)#7天周日期差
+    # difference=0
+
+    for i in range((dt2 - dt1).days + 1):
+        # print(i)
+        day = dt1 + datetime.timedelta(days=i)
+        bianli = day.isoweekday()
+        if bianli == 7:
+            print(day)
+        else:
+            continue
+all_sundays(a)
