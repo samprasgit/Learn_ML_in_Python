211cef0c2c9186cb821436058109bfb07abfa8de
samprasgit <by14274044@gmail.com>
2020-08-14 10:58:44 +0800

    Update

--- Python基础知识梳理/Pyhton-Language/test.py
+++ Python基础知识梳理/Pyhton-Language/test.py
@@ -1,9 +1,11 @@
-try:
-    f = open('将进酒.txt', 'w')
-    for line in f:
-        print(line)
-except OSError as error:
-    print("出错啦！%s" % str(error))
 
-finally:
-    f.close()
+train = ["我 爱 北 京 天 安 门", "我 喜 欢 上 海"]
+train_bigarm = []
+for value in train:
+    lentext = len(value.split())
+    value = value.split()
+    for i in range(lentext - 1):
+        value.append("".join(value[i:i + 2]))
+
+    train_bigarm.append(" ".join(value))
+print(train_bigarm)
