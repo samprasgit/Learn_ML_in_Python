448076df7d7082e472f6bcb2ceac905a9eeeb617
samprasgit <by14274044@gmail.com>
2020-10-18 15:22:10 +0800

    UPdate

---
+++ 算法设计基础/剑指offer/python/顺时针打印矩阵.py
@@ -0,0 +1,33 @@
+class Solution:
+
+    def spiralOrder(self, matrix):
+        '''  
+
+        模拟
+        '''
+        if not matrix:
+            return []
+        l, r, t, b, res = 0, len(matrix[0]) - 1, 0, len(matrix) - 1, []
+        while True:
+            for i in range(l, r + 1):
+                res.append(matrix[t][i])
+            t += 1
+            if t > b:
+                break
+            for i in range(t, b + 1):
+                res.append(matrix[i][r])
+            r -= 1
+            if l > r:
+                break
+            for i in range(r, l - 1, -1):
+                res.append(matrix[b][i])
+            b -= 1
+            if t > b:
+                break
+            for i in range(b, t - 1, -1):
+                res.append(matrix[i][l])
+            l += 1
+            if l > r:
+                break
+
+        return res
--- 算法设计基础/剑指offer/test.py
+++ 算法设计基础/剑指offer/test.py
@@ -1,1 +1,10 @@
+from heapq import heappush, heappop
+heap = []
+heappush(heap, 1)
+heappush(heap, 4)
+heappush(heap, 2)
+heappush(heap, 6)
+heappush(heap, 8)
 
+
+heappop(heap)
