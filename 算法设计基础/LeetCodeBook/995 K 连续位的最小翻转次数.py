f2b999e10a34a6ba37916e037a631e62f0ee6bea
samprasgit <by14274044@gmail.com>
2021-02-21 01:23:13 +0800

    Create 995 K 连续位的最小翻转次数.py

---
+++ 算法设计基础/LeetCodeBook/995 K 连续位的最小翻转次数.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Time : 2021/2/18 1:06 下午
+# Author : samprasgit
+# desc :
+
+class Solution:
+    def minKBitFlips(self,A,K):
+        """
+        滑动窗口
+        """
+        
+
